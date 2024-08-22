19ed81df6dd030940e2117ad3260ad0716d89a0b
raventid <juliankul@gmail.com>
2024-08-22 14:42:52 +0800

    update orderbookv2

--- src/orderbookv2.rs
+++ src/orderbookv2.rs
@@ -17,7 +17,31 @@ struct LevelInfo {
     quantity: Quantity,
 }
 
-struct LevelInfos(Vec<LevelInfo>);
+struct OrderBookLevelInfos {
+    bids: Vec<LevelInfo>,
+    asks: Vec<LevelInfo>,
+}
+
+impl OrderBookLevelInfos {
+    fn new(bids: Vec<LevelInfo>, asks: Vec<LevelInfo>) -> OrderBookLevelInfos {
+        OrderBookLevelInfos { bids, asks }
+    }
+
+    fn from_existing() -> OrderBookLevelInfos {
+        OrderBookLevelInfos {
+            bids: Vec::new(),
+            asks: Vec::new(),
+        }
+    }
+
+    fn get_bids(&self) -> &Vec<LevelInfo> {
+        &self.bids
+    }
+
+    fn get_asks(&self) -> &Vec<LevelInfo> {
+        &self.asks
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -28,5 +52,13 @@ mod tests {
         let Price = 10;
 
         assert_eq!(Price, 10);
+    }
+
+    #[test]
+    fn test_orderbooklevelinfos() {
+        let orderbooklevelinfos = OrderBookLevelInfos::from_existing();
+
+        assert_eq!(orderbooklevelinfos.bids.len(), 0);
+        assert_eq!(orderbooklevelinfos.asks.len(), 0);
     }
 }
